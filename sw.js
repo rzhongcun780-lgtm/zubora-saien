72c7ff04f43383201a5602ac9184be83754739e6
rzhongcun780-lgtm <rzhongcun780@gmail.com>
2026-03-25 21:25:03 +0900

    SW v3: self-destruct, clear all caches

--- sw.js
+++ sw.js
@@ -1,16 +1,11 @@
-const CACHE_NAME = 'zubora-v2';
-const OLD_CACHES = ['zubora-v1'];
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(['/zubora-saien/'])));
-  self.skipWaiting();
-});
+// SW v3 - self-destruct: clears all caches and unregisters
+self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => OLD_CACHES.includes(k)).map(k => caches.delete(k)))
-    ).then(() => self.clients.claim())
+    caches.keys()
+      .then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.registration.unregister())
+      .then(() => self.clients.matchAll())
+      .then(clients => clients.forEach(c => c.navigate(c.url)))
   );
-});
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
