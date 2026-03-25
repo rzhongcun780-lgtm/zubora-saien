689ca8657dd9ccbc77a8e20c10d9d63fa4c29f71
rzhongcun780-lgtm <rzhongcun780@gmail.com>
2026-03-25 20:50:35 +0900

    Update SW: bump to v2, clear old cache

--- sw.js
+++ sw.js
@@ -1,6 +1,15 @@
-const CACHE_NAME = 'zubora-v1';
+const CACHE_NAME = 'zubora-v2';
+const OLD_CACHES = ['zubora-v1'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(['/zubora-saien/'])));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => OLD_CACHES.includes(k)).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
 });
 self.addEventListener('fetch', e => {
   e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
